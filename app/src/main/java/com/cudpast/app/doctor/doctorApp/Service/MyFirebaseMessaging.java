46ee10e37223c5101e6700b96af7c3ffa1763b32
Slinkter <luis.j.cueva@gmail.com>
2019-02-20 10:43:05 -0500

    Miercoles : mejoras de Notificaciones

--- app/src/main/java/com/cudpast/app/doctor/doctorApp/Service/MyFirebaseMessaging.java
+++ app/src/main/java/com/cudpast/app/doctor/doctorApp/Service/MyFirebaseMessaging.java
@@ -12,31 +12,29 @@ import com.google.gson.Gson;
 
 public class MyFirebaseMessaging extends FirebaseMessagingService {
 
-    public static final String TAG ="MyFirebaseMessaging";
+    public static final String TAG = "MyFirebaseMessaging";
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
 
-        LatLng customer_location = new Gson().fromJson(remoteMessage.getNotification().getBody(),LatLng.class);
+        LatLng customer_location = new Gson().fromJson(remoteMessage.getNotification().getBody(), LatLng.class);
         Token pacienteToken = new Gson().fromJson(remoteMessage.getNotification().getBody(), Token.class);
 
         Intent intent = new Intent(getBaseContext(), CustomerCallActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        intent.putExtra("lat",customer_location.latitude);
-        intent.putExtra("lng",customer_location.longitude);
-        intent.putExtra("customer",remoteMessage.getNotification().getTitle());
+        intent.putExtra("lat", customer_location.latitude);
+        intent.putExtra("lng", customer_location.longitude);
+        intent.putExtra("customer", remoteMessage.getNotification().getTitle());
+        intent.putExtra("lat", customer_location.latitude);
+        intent.putExtra("lng", customer_location.longitude);
+        intent.putExtra("tokenPaciente", remoteMessage.getNotification().getTitle());
 
-        intent.putExtra("lat",customer_location.latitude);
-        intent.putExtra("lng",customer_location.longitude);
-        intent.putExtra("tokenPaciente",remoteMessage.getNotification().getTitle());
 
-
-        Log.e(TAG, "From  : " + remoteMessage.getFrom());
-        Log.e(TAG, "Notification Message title  tokenPaciente : " + remoteMessage.getNotification().getTitle().toString());
-        Log.e(TAG, "Notification Message Body  lat/lng: " + remoteMessage.getNotification().getBody().toString());
-        Log.e(TAG, "Notification customer_location : " + customer_location);
-
+//        Log.e(TAG, "From  : " + remoteMessage.getFrom());
+//        Log.e(TAG, "Notification Message title  tokenPaciente : " + remoteMessage.getNotification().getTitle().toString());
+//        Log.e(TAG, "Notification Message Body  lat/lng: " + remoteMessage.getNotification().getBody().toString());
+//        Log.e(TAG, "Notification customer_location : " + customer_location);
 
         startActivity(intent);
     }
