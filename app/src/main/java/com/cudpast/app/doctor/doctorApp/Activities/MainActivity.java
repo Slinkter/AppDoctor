c9994d1c03c48e0efc2d5bf14bd5bb392fd6192a
Slinkter <luis.j.cueva@gmail.com>
2019-05-21 11:47:43 -0500

    version 1.12

--- app/src/main/java/com/cudpast/app/doctor/doctorApp/Activities/MainActivity.java
+++ app/src/main/java/com/cudpast/app/doctor/doctorApp/Activities/MainActivity.java
@@ -41,6 +41,8 @@ public class MainActivity extends AppCompatActivity implements
     TextView especialidadDoctor;
 
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
