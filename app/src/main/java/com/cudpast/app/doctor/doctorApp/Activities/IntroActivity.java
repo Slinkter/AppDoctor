6b0ee5bdb5afa880427e740c49c9f4fa17955e1c
Slinkter <luis.j.cueva@gmail.com>
2019-05-24 16:49:58 -0500

    2019 05 24 :(Viernes) Funciona el AppIntro

---
+++ app/src/main/java/com/cudpast/app/doctor/doctorApp/Activities/IntroActivity.java
@@ -0,0 +1,41 @@
+package com.cudpast.app.doctor.doctorApp.Activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+
+import com.cudpast.app.doctor.doctorApp.R;
+import com.cudpast.app.doctor.doctorApp.Soporte.SampleSlide;
+import com.github.paolorotolo.appintro.AppIntro;
+
+public class IntroActivity extends AppIntro {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addSlide(SampleSlide.newInstance(R.layout.slide_1));
+        addSlide(SampleSlide.newInstance(R.layout.slide_2));
+        addSlide(SampleSlide.newInstance(R.layout.slide_3));
+
+
+
+
+    }
+
+    @Override
+    public void onSkipPressed(Fragment currentFragment) {
+        super.onSkipPressed(currentFragment);
+        finish();
+    }
+
+    @Override
+    public void onDonePressed(Fragment currentFragment) {
+        super.onDonePressed(currentFragment);
+        finish();
+    }
+
+    @Override
+    public void onSlideChanged(@Nullable Fragment oldFragment, @Nullable Fragment newFragment) {
+        super.onSlideChanged(oldFragment, newFragment);
+    }
+}
